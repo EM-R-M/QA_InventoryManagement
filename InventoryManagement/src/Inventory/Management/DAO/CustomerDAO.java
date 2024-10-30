e374d1cd0571821206f52ce86aebc17cf9263ae5
EM-R_M <emily.manton@aol.com>
2024-10-30 13:39:19 +0000

    merge before refactoring

--- InventoryManagement/src/Inventory/Management/DAO/CustomerDAO.java
+++ InventoryManagement/src/Inventory/Management/DAO/CustomerDAO.java
@@ -8,7 +8,7 @@ import java.sql.*;
 public class CustomerDAO {
 
     // Variable for database connections
-    DatabaseConnection inventoryDatabase = new DatabaseConnection("jdbc:mysql://localhost:3306/inventorymanagement", "root", "root");
+    DatabaseConnection inventoryDatabase = new DatabaseConnection();
 
     // Add a new customer to the list
     public void addCustomer(Customer newCustomer){
--- InventoryManagement/src/Inventory/Management/Persistence/DatabaseConnection.java
+++ InventoryManagement/src/Inventory/Management/Persistence/DatabaseConnection.java
@@ -4,18 +4,13 @@ import java.sql.*;
 
 public class DatabaseConnection {
 
-    private String jdbcConnectionURL;
-    private String username;
-    private String password;
-
-    public DatabaseConnection(String jdbcConnectionURL, String username, String password) {
-        this.jdbcConnectionURL = jdbcConnectionURL;
-        this.username = username;
-        this.password = password;
-    }
-
     public Connection connect() throws SQLException, ClassNotFoundException {
         Class.forName("com.mysql.cj.jdbc.Driver");
+
+        String jdbcConnectionURL = "jdbc:mysql://localhost:3306/inventorymanagement";
+        String username = "root";
+        String password = "root";
+
         return DriverManager.getConnection(jdbcConnectionURL, username, password);
     }
 
